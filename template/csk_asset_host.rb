29ba845cab73c47eb4fe20f56bd192587ff8f7a9
Lenny Sirivong <lenny.sirivong@voxmedia.com>
2020-12-02 09:43:48 -0700

    Rename AssetHost, implement filename rewriting, add extension to config

--- template/csk_asset_host.rb
+++ template/csk_asset_host.rb
@@ -1,12 +1,14 @@
+# fork of core AssetHost extension
 # https://github.com/middleman/middleman/blob/v4.3.10/middleman-core/lib/middleman-core/extensions/asset_host.rb
 require 'addressable/uri'
 
-class Middleman::Extensions::AssetHost < ::Middleman::Extension
+class CskAssetHost < ::Middleman::Extension
   option :host, nil, 'The asset host to use or a Proc to determine asset host', required: true
   option :exts, nil, 'List of extensions that get cache busters strings appended to them.'
   option :sources, %w(.css .htm .html .js .php .xhtml), 'List of extensions that are searched for bustable assets.'
   option :ignore, [], 'Regexes of filenames to skip adding query strings to'
   option :rewrite_ignore, [], 'Regexes of filenames to skip processing for host rewrites'
+  option :asset_path_rewriter, nil, 'Proc for modifying the asset path'
 
   def initialize(app, options_hash={}, &block)
     super
@@ -30,6 +32,8 @@ class Middleman::Extensions::AssetHost < ::Middleman::Extension
       asset_path
     end
 
+    full_asset_path = sanitize_full_asset_path(full_asset_path)
+
     asset_prefix = if options[:host].is_a?(Proc)
       options[:host].call(full_asset_path)
     elsif options[:host].is_a?(String)
@@ -39,4 +43,15 @@ class Middleman::Extensions::AssetHost < ::Middleman::Extension
     File.join(asset_prefix, full_asset_path)
   end
   memoize :rewrite_url
+
+  private
+  def sanitize_full_asset_path full_asset_path
+    parts = full_asset_path.split('/')
+    path_prefix = parts[0...-1]
+    filename = parts[-1]
+    # Duplicate the sanitization done by the asset host (replace non-word characters with `_`)
+    sanitized_filename = filename.gsub(/[^.\w]/, '_')
+
+    (path_prefix + [sanitized_filename]).join('/')
+  end
 end
